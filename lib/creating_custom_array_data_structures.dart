73304f4292fa7b634d2d6d45403a403abcf82ee5
Tarikul-Tuhin <tituhinkh@gmail.com>
2022-07-15 23:13:30 +0600

    custom array

--- lib/creating_custom_array_data_structures.dart
+++ lib/creating_custom_array_data_structures.dart
@@ -1,24 +1,28 @@
-//in this class we have showed how to create custom array on our own. we can build any data structure on our own like this.
+//in this class we have showed how to create custom array on our own. we can build any data structure on our own like this. Time complexity attached with every method in this data structure.
 
 class CustomArray {
   int length = 0;
   var data = {};
 
   getIndexWiseValue(index) {
+    // Big O (1)
     return 'index $index is ${data[index]}';
   }
 
   getListAndLength() {
+    // Big O (1)
     return {length, data};
   }
 
   push(item) {
+    // Big O (1)
     data[length] = item;
     length++;
     return length;
   }
 
   pop() {
+    // Big O (1)
     final deletedItem = data[length - 1];
     data.remove(length -
         1); // it will remove by key of the Last index. if length-1 is 1, it will search the key which is 1, and if it finds, then it will remove it from the object
@@ -27,6 +31,7 @@ class CustomArray {
   }
 
   deleteIndexWise(index) {
+    // Big O (n)
     final item = data[index];
     // data.remove(index);
     // length--;
@@ -42,3 +47,14 @@ class CustomArray {
     return 'Deleted index $index which is $item';
   }
 }
+
+
+/*
+
+Array  ==> push - O(1)
+           pop - O(1)
+           insert - O(n)  ==> because, we need to shift each index after the inserting index
+           delete - O(n)  ==> because, we need to shift each index after the deleting index
+
+
+*/
